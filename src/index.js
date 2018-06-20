c7b3fb8614cb75d09e2ab5a9e0c159f43b8a50af
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-20 18:21:28 -0400

    add bootstrap as a dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
+
 
 import './index.css';
 import IdeasIndex from './components/ideas_index'
