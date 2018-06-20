7a2af4e2cfa48675c12f8fe4842285cf0658ad29
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-20 18:16:01 -0400

    1. render ideasList. 2. install react-bootstrap.

--- src/components/ideas_index.js
+++ src/components/ideas_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchIdeas } from '../actions'
@@ -8,12 +9,23 @@ class IdeasIndex extends Component {
     }
     // this fun will run immed after this component is rendered
     // which go and fetch some data or initiate one time loading process
-
+    renderIdeas() {
+        return _.map(this.props.ideas, idea => {
+            return (
+                <li className="list-group-item" key={idea.id}>
+                    {idea.title}
+                </li>
+            )
+        })
+    }
     render() {
-        console.log(this.props.ideas)
+        // console.log(this.props.ideas)
         return(
             <div>
-                #IdeasIndex 
+                <h3>Ideas</h3>
+                <ul className="list-group">
+                    {this.renderIdeas()}
+                </ul>
             </div>
         )
     }
