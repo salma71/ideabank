3582585e63435c73f0196a7e06720bb31f807dd3
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-20 10:18:42 -0400

    render ideasList component

---
+++ src/components/ideas_list.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class IdeasList extends Component {
+    render() {
+        return(
+            <div>
+                Ideas 
+            </div>
+        )
+    }
+}
+
+export default IdeasList;
