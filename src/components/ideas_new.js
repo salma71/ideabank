3bd85c08d720b567457103e82e52e31bea84a84e
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-21 08:48:48 -0400

    1.use redux-form to create a form 2.render input form on the new route

--- src/components/ideas_new.js
+++ src/components/ideas_new.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'  // similar to connect helper
 
 class IdeasNew extends Component {
+    renderTitleField(field) {
+        // return some amount of JSX
+        // this field argument have some props and eventhandlers we should be able to wire it up
+        return (
+            <div>
+                <input 
+                type="text"
+                {...field.input}
+                />
+            </div>
+        )
+    }
+
     render() {
         return(
-            <div>IdeaNew</div>
+            <div>
+                <form>
+                    <Field 
+                    // this Field component is know how to deal with Redux form only 
+                    // don't know how to show myself on the screen
+                    name="title" //what piece of state the user want to create
+                    component={this.renderTitleField} //takes fun to display the component - help Field to 
+                    // show himself on the screen
+                    />
+                </form>
+            </div>
         )
     }
 }
 
-export default IdeasNew;
+export default reduxForm({
+    // some config options
+    form: 'IdeasNewForm' //as name of the form - this name should be unique
+})(IdeasNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 // import { reducer as formReducer } from 'redux-form';
 //just renaming reducer to formreducer
+import { reducer as formReducer } from 'redux-form'
 import IdeaReducer from './reducer_ideas';
 
 const rootReducer = combineReducers({
     // state: (state = {}) => state
-    ideas: IdeaReducer
-    // form: formReducer
+    ideas: IdeaReducer,
+    form: formReducer
 });
 
 export default rootReducer;
