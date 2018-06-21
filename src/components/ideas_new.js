e59d931d0caa192e62c66b14684817739b9b0fbb
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-21 11:18:21 -0400

    refactor field helper method into dynamic label

--- src/components/ideas_new.js
+++ src/components/ideas_new.js
@@ -2,29 +2,16 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'  // similar to connect helper
 
 class IdeasNew extends Component {
-    renderTitleField(field) {
+    renderField(field) {
         // return some amount of JSX
         // this field argument have some props and eventhandlers we should be able to wire it up
         return (
             <div className="form-group">
-                <label>Title</label>
+                <label>{field.label}</label>
                 <input 
                 className="form-control"
                 type="text"
                 {...field.input}
-                />
-            </div>
-        )
-    }
-
-    renderBodyField(field) {
-        return (
-            <div className="form-group">
-                <label>Body:</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    {...field.input}
                 />
             </div>
         )
@@ -37,13 +24,15 @@ class IdeasNew extends Component {
                     <Field 
                     // this Field component is know how to deal with Redux form only 
                     // don't know how to show myself on the screen
+                    label="Title:"
                     name="title" //what piece of state the user want to create
-                    component={this.renderTitleField} //takes fun to display the component - help Field to 
+                    component={this.renderField} //takes fun to display the component - help Field to 
                     // show himself on the screen
                     />
                     <Field 
-                        name="body"
-                        component={this.renderBodyField}
+                    label="Body:"
+                    name="body"
+                    component={this.renderField}
                     />
                 </form>
             </div>
